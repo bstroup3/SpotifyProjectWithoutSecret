106889cb955b580cb588286bedeec4a404d0b9aa
Ben Stroup <bstroup@iastate.edu>
2020-08-20 12:21:31 -0500

    fix???

--- Spot_App.py
+++ Spot_App.py
@@ -3,15 +3,16 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
-SPOTIPY_CLIENT_ID = '821a2836760b4990b1bec51520a7bc43'
-SPOTIPY_CLIENT_SECRET = 'bcef5b3d0b9646a685b9b82c03399756'
+SPOTIPY_CLIENT_ID = "821a2836760b4990b1bec51520a7bc43"
+SPOTIPY_CLIENT_SECRET = "bcef5b3d0b9646a685b9b82c03399756"
+REDIRECT_URI = "http://localhost:8888/callback"
 user = "bds425"
 
 client_credentials_manager = SpotifyClientCredentials(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
-scope = 'user-library-read playlist-read-private'
-token = util.prompt_for_user_token(user, scope)
+scope = "user-read-email"
+token = util.prompt_for_user_token(user, scope, client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=REDIRECT_URI)
 
 if token:
     sp = spotipy.Spotify(auth=token)
