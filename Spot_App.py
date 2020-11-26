9fde1c9d68ca89e342d6bb00841dd836fb395511
Ben Stroup <bstroup@iastate.edu>
2020-11-26 15:22:03 -0600

    Added remove songs function

--- Spot_App.py
+++ Spot_App.py
@@ -2,6 +2,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
+import tkinter as tk
 
 SPOTIPY_CLIENT_ID = "821a2836760b4990b1bec51520a7bc43"
 SPOTIPY_CLIENT_SECRET = "bcef5b3d0b9646a685b9b82c03399756"
@@ -32,18 +33,23 @@ def view_owned_playlists():
     print("What playlist would you like to view?")
     request = input()
 
-    playlist = sp.playlist(playlist_id[request])
-    print("Playlist Name:")
-    print(playlist["name"])
-    print("Songs:")
-    for x in playlist["tracks"]["items"]:
-        track = x["track"]["name"]
-        print(track)
+    try:
+        playlist = sp.playlist(playlist_id[request])
+        print("Playlist Name:")
+        print(playlist["name"])
+        print("Songs:")
+        for x in playlist["tracks"]["items"]:
+            track = x["track"]["name"]
+            print(track)
+    finally:
+        print("Playlist not found")
+        return
 
 def create_new_playlist():
     print("What would you like to name the playlist?")
     playlist_name = input()
     sp.user_playlist_create(user, playlist_name, True)
+    print("playlist created")
 
 def view_followed_artists():
     followed_artists = sp.current_user_followed_artists()
@@ -64,10 +70,48 @@ def add_items_to_playlist():
         print(name)
     print("Which song/songs would you like to add?")
     add_song = input()
-    idOFSong = song_id[add_song]
+    idOfSong = song_id[add_song]
     randomList = []
-    randomList += [idOFSong]
+    randomList += [idOfSong]
     sp.user_playlist_add_tracks(user, playlist_id[playlist], randomList)
+    print("song/songs added")
+
+def remove_songs_from_playlist():
+    print("which playlist would you like to remove songs from?")
+    request = input()
+
+    try:
+        playlist = sp.playlist(playlist_id[request])
+        print("Playlist Name:")
+        print(playlist["name"])
+        print("Songs:")
+        song_id = {}
+        for x in playlist["tracks"]["items"]:
+            track = x["track"]["name"]
+            id = x["track"]["id"]
+            song_id[track] = id
+            print(track)
+
+        removed_songs = []
+
+        while 1:
+            print("Which song would you like to remove?")
+            song = input()
+            idOfSong = song_id[song]
+            removed_songs += [idOfSong]
+            print("would you like to remove another song?")
+            answer = input()
+            if answer == "yes" or answer == "Yes":
+                continue
+            else:
+                break
+        sp.user_playlist_remove_all_occurrences_of_tracks(user, playlist_id[request], removed_songs)
+        print("song/songs removed")
+
+    finally:
+        print("Could not find Playlist")
+        return
+
 
 def add_image_to_playlist():
     print("What playlist?")
@@ -77,6 +121,7 @@ def add_image_to_playlist():
     requestImage = input()
     addInto = playlist_id[request]
     sp.playlist_upload_cover_image(playlist_id=addInto, image_b64=requestImage)
+    print("Image added")
 
 
 
@@ -94,7 +139,8 @@ while 1:
     print("2. View followed artists")
     print("3. Make a new playlist")
     print("4. Add songs to a playlist")
-    print("5. Add cover image to playlist")
+    print("5. Remove songs from a playlist")
+    print("6. Add cover image to playlist")
     print("Pick a number:")
     Request = input()
 
@@ -104,17 +150,15 @@ while 1:
         view_followed_artists()
     elif Request == "3":
         create_new_playlist()
-        print("playlist created")
     elif Request == "4":
         add_items_to_playlist()
-        print("song/songs added")
     elif Request == "5":
+        remove_songs_from_playlist()
+    elif Request == "6":
         add_image_to_playlist()
-        print("Image added")
     else:
         print("That is not an option")
         print()
-        continue
 
     print()
     print("Would you like to do something else?")
